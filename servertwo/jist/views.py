9e3aaaf7b8e95e289046921aab7516023798f614
joedonovan <joepeterdonovan@gmail.com>
2023-05-23 10:25:15 -0500

    trascript saved in db

--- servertwo/jist/views.py
+++ servertwo/jist/views.py
@@ -43,29 +43,16 @@ class AudioUploadView(View):
             audio_segment.export(output_file_path, format='wav')
             print('Saved file to: ', output_file_path)
 
-
-            # # make api request to Whisper Ai for transcription 
-            # whisper_api_url = 'https://api.openai.com/v1/audio/transcriptions'
-            # headers ={
-            #     'Authorization': "Bearer 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",
-            #     'Content-Type': 'audio/wav'
-            #     }
-            # files = {'audio': open(output_file_path, 'rb')}
-
-
-            # # make api request for Whisper
-            # response = requests.post(whisper_api_url, headers=headers, files=files)
-            # transcribe audio using whisper
-            
-            # result = whisper.transcribe(output_file_path)
-            # print(result['text'])
-
             model = whisper.load_model("base")
             result = model.transcribe(output_file_path)
+            transcript_text = result['text']
             print(result["text"])
 
-    
-            
+            # save audio and trasncript to database 
+            audio = Audio()
+            audio.audio_file.save(output_filename, audio_file)
+            audio.transcribe = transcript_text
+            audio.save()
 
             return JsonResponse({'message': 'Audio conversation and save completed'})
         else: 
