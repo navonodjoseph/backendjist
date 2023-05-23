9c898b76875a398cab6ab08d4e31d786262ad922
joedonovan <joepeterdonovan@gmail.com>
2023-05-22 19:20:03 -0500

    uploading a first attempt at transcription

--- servertwo/whisper/views.py
+++ servertwo/whisper/views.py
@@ -14,6 +14,7 @@ import io
 import os
 import requests
 import datetime
+import whisper
 
 
 class AudioUploadView(View):
@@ -37,46 +38,33 @@ class AudioUploadView(View):
             timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
             output_filename = f'audio_{timestamp}.wav'
 
-            # save audio as wav.file
+            # save audio as .wavfile
             output_file_path = os.path.join(output_folder, output_filename)
             audio_segment.export(output_file_path, format='wav')
             print('Saved file to: ', output_file_path)
 
-            # transcribe audio
-            transcriptions_folder = 'transcriptions'
-            if not os.path.exists(transcriptions_folder):
-                os.makedirs(transcriptions_folder)
+
+            # # make api request to Whisper Ai for transcription 
+            # whisper_api_url = 'https://api.openai.com/v1/audio/transcriptions'
+            # headers ={
+            #     'Authorization': "Bearer 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",
+            #     'Content-Type': 'audio/wav'
+            #     }
+            # files = {'audio': open(output_file_path, 'rb')}
+
+
+            # # make api request for Whisper
+            # response = requests.post(whisper_api_url, headers=headers, files=files)
+            # transcribe audio using whisper
             
-            transcription_filename = f'transcription_{timestamp}.txt'
-            transcription_filepath = os.path.join(transcriptions_folder, transcription_filename)
+            model = whisper.load_model("base")
+            transcribe = model.transcribe('output_filepath')
+            print(transcribe['text'])
 
-            # make api request to Whisper Ai for transcription 
-            whisper_api_url = 'https://api.openai.com/v1/audio/transcriptions'
-            headers ={
-                'Authorization': "Bearer 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",
-                'Content-Type': 'audio/wav'
-                }
-            files = {'audio': open(output_file_path, 'rb')}
 
-            transcription = ''
 
-            # make api request for Whisper
-            response = requests.post(whisper_api_url, headers=headers, files=files)
-
-            if response.status_code==200:
-                transcription = response.text
+    
             
-            # save transcription to a file
-            if transcription: 
-                output_file_path = os.path.join(output_folder, output_filename)
-                with open(transcription_filepath, 'w') as file:
-                    file.write(transcription)
-            
-                print('Transcription saved to:', transcription_filepath)
-            else: print('No transcription available')
-            # create a record in the database
-            audio_file_record = Audio(transcribe=transcription_filepath)
-            audio_file_record.save()
 
             return JsonResponse({'message': 'Audio conversation and save completed'})
         else: 
