fc0e830f58a40f0016e2d1e38d6a7ee002c08315
joedonovan <joepeterdonovan@gmail.com>
2023-05-24 20:12:38 -0500

    fixing settings.py for heroku launch

--- backendjist/settings.py
+++ backendjist/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders', 
+    'django-cors-headers', 
     'jist',
     'rest_framework', 
-    'whisper'
+
 ]
 
 MIDDLEWARE = [
