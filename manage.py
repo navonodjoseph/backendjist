b3951ffac7914ec9e93251dd423e03024ddb24cf
joedonovan <joepeterdonovan@gmail.com>
2023-05-24 22:42:57 -0500

    changed manage.py

--- manage.py
+++ manage.py
@@ -14,8 +14,17 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    ## added line 18
+    port = int(os.environ.get('PORT', 5000))
+    
+    # commented out line 21
+    # execute_from_command_line(sys.argv)
 
+    #added line 24-5
+    if len(sys.argv) > 1 and sys.argv[1] =='runserver':
+        sys.argv.append('0.0.0.0:{}'.format(port))
+
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
